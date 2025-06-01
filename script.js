cb39f8f77b0d04b601d89394b8995220d59e9f2f
Anica Rahman <anicarahman2009@yahoo.it>
2025-06-01 17:28:05 +0100

    Carousel added

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//menu mobile
 const toggle = document.querySelector(".menu-toggle");
 const mobileMenu = document.getElementById("mobileMenu");
 
@@ -11,4 +12,17 @@ window.addEventListener("resize", () => {
   if (window.innerWidth > 768) {
     mobileMenu.classList.remove("active");
   }
+});
+
+// Home carousel slider
+const elem = document.querySelector(".main-carousel");
+const flkty = new Flickity(elem, {
+  cellAlign: "left",
+  contain: true,
+  wrapAround: true,
+  prevNextButtons: true,
+  imagesLoaded: true,
+  lazyLoad: true,
+  autoPlay: 3000,
+  pauseAutoPlayOnHover: true,
 });
